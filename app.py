6d8ae114fa3e05dcdc05163b5a334a9acadb4547
maesaetern <msaetern28@wooster.edu>
2026-04-05 17:49:43 -0400

    Import Flask Import request Import jsonify
    This will create the server that can be accessed in the terminal / command line by running python app.py
    Then retrieve data by using curl http://127.0.0.1:5000/

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return jsonify({'message': 'hello flask'})
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
