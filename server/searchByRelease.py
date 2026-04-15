72688cd83842f2be92397adb8c7b555afd8b5198
Zachary Butler <254991337+Zbutler798@users.noreply.github.com>
2026-04-15 14:24:17 -0400

    Creates searchByRelease.py file

---
+++ server/searchByRelease.py
@@ -0,0 +1,15 @@
+from flask import current_app
+from flask import request
+import requests
+
+API_KEY: str = current_app.config.get("TMDB_API_KEY")
+TMDB_URL: str = current_app.config.get("TMDB_URL")
+
+print(TMDB_URL)
+
+@current_app.get('/search_by_release')
+def search_by_release(date): # Enter in format: YEAR-MO-DY (EX. 2026-04-13)
+    """Returns a list of movies released on the specified date"""
+    
+    data = requests.get("f{TMDB_URL}discover/movie?api_key={API_KEY}&primary_release_date.lte={date}&primary_release_date.gte={date}")
+    print(data.json["results"])
