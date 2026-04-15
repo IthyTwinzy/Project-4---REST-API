f0359dea4052cc878801f8b5f64e78c617788842
Zachary Butler <254991337+Zbutler798@users.noreply.github.com>
2026-04-15 14:46:14 -0400

    searchByRelease.py function added

--- server/searchByRelease.py
+++ server/searchByRelease.py
@@ -5,11 +5,10 @@ import requests
 API_KEY: str = current_app.config.get("TMDB_API_KEY")
 TMDB_URL: str = current_app.config.get("TMDB_URL")
 
-print(TMDB_URL)
-
 @current_app.get('/search_by_release')
-def search_by_release(date): # Enter in format: YEAR-MO-DY (EX. 2026-04-13)
+def search_by_release(): # Enter in format: YEAR-MO-DY (EX. 2026-04-13)
     """Returns a list of movies released on the specified date"""
+    date = requests.args.get("date")
     
     data = requests.get("f{TMDB_URL}discover/movie?api_key={API_KEY}&primary_release_date.lte={date}&primary_release_date.gte={date}")
     print(data.json["results"])
